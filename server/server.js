4e1419d65f2bd8ebcc293f3b1a40d9ba6321afcf
Ihab <ihab@epicom.com>
2019-09-28 21:40:51 -0500

    setup server.js

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const rockets = require("./routes/api/rockets");
+
+const app = express();
+
+//Connect Database
+connectDB();
+
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Register api routes
+app.use("/api/rockets", rockets);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
