6cf5ef77ec202aaa17f7337d7e4df04531425321
Ihab <ihab@epicom.com>
2019-09-30 22:44:50 -0500

    move route logic to a separate model

--- server/routes/api/rockets.js
+++ server/routes/api/rockets.js
@@ -1,15 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const Rocket = require("../../models/Rockets");
+const RocketsCtrl = require("../controller/rockets");
 
-router.get("/", async (req, res) => {
-  try {
-    const rockets = await Rocket.find().sort({ flight_number: 1 });
-    res.json(rockets);
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("Something went wrong");
-  }
-});
+router.get("/", RocketsCtrl.apiGetRockets);
 
 module.exports = router;
